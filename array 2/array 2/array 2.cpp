a88eca1770d8d1d5e0f27867ac7ebe2c9334ef55
Rawan Sameh <Rawansamehhosny1@gmail.com>
2025-11-15 20:49:25 +0200

    alot of loops in there

--- array 2/array 2/array 2.cpp
+++ array 2/array 2/array 2.cpp
@@ -1,11 +1,26 @@
-// array 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <limits>
 using namespace std;
+
 int main()
 {
-	juuuu
+    const int size = 3;
+    float marks[size];
 
+    for (int i = 0; i < size; i++) {
+        cout << "Enter marks " << i + 1 << ": ";
+        while (!(cin >> marks[i])) {
+            cout << "invalid input, we told u to enter a number!" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    float sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += marks[i];
+        cout << sum << endl;
+    }
+
+    cout << "The average of your marks is: " << sum / size << endl;
 }
-

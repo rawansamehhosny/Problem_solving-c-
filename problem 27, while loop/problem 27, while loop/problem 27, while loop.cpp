add71483660a7df243829843499fffb78d336b9e
Rawan Sameh <Rawansamehhosny1@gmail.com>
2025-11-22 00:10:17 +0200

    problem 27 new validation

--- problem 27, while loop/problem 27, while loop/problem 27, while loop.cpp
+++ problem 27, while loop/problem 27, while loop/problem 27, while loop.cpp
@@ -3,10 +3,29 @@
 
 #include <iostream>
 using namespace std;
+
+void validation (int &number)
+{
+    while (true) {
+		cout << "Please enter a positive number: ";
+        cin >> number;
+        if (cin.fail()) {
+			cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input. ";
+			continue;
+        }
+        if (number <= 0) {
+			cout << "The number must be positive. ";
+			continue;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        break;
+}
+}
 int main()
 {
-    cout << "Enter a number: ";
-    int n; cin >> n;
+    int n; validation(n);
     int counter = n;
     while (counter > 1)
     {

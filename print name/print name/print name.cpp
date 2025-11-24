0aea9a52936333f332e193cbad9e188b25da2ee1
Rawan Sameh <Rawansamehhosny1@gmail.com>
2025-11-24 11:21:09 +0200

    function

---
+++ print name/print name/print name.cpp
@@ -0,0 +1,16 @@
+// print name.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void printname(string name) {
+	cout << "your name is: " << name << endl;
+}
+int main()
+{
+
+	printname("Rawan sameh");
+
+}
+
